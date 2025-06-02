d528a0ae84b705f0d64ff881a58bc90990cb4c02
lcarsos <lcarsos@lcarsos.com>
2025-06-01 23:05:21 -0600

    displaying diags again, and configuring some lsp keys

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -79,6 +79,57 @@ require("lazy").setup({
       },
       'hrsh7th/cmp-nvim-lsp',
     },
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+
+          local tb = require('telescope.builtin')
+
+          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', {'n', 'x'})
+          map('grr', tb.lsp_references, '[G]oto [R]eferences')
+          map('gri', tb.lsp_implementations, '[G]oto [I]mplementation')
+          map('grd', tb.lsp_definitions, '[G]oto [D]efinition')
+          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('gO', tb.lsp_document_symbols, '[O]pen Document Symbols')
+          map('gW', tb.lsp_dynamic_workspace_symbols, 'Open [W]orkspace Symbols')
+          map('grt', tb.lsp_type_definitions, '[G]oto [T]ype Definition')
+
+          map('gl', vim.diagnostic.open_float, 'Show Warnings')
+
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf ) then
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          end
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf ) then
+          end
+        end,
+      })
+
+      vim.diagnostic.config {
+        severity_sort = true,
+        float = { border = 'rounded', source = 'if_many' },
+        underline = {severity = vim.diagnostic.severity.ERROR },
+        virtual_text = {
+          source = 'if_many',
+          spacing = 2,
+          format = function(diagnostic)
+            local diagnostic_message = {
+              [vim.diagnostic.severity.ERROR] = diagnostic.message,
+              [vim.diagnostic.severity.WARN] = diagnostic.message,
+              [vim.diagnostic.severity.INFO] = diagnostic.message,
+              [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+          end,
+        },
+      }
+    end,
   },
 
   -- Completions

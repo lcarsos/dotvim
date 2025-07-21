631f327b5580b126556e8bdcca0529365a6518ca
lcarsos <lcarsos@lcarsos.com>
2025-07-21 16:29:20 -0600

    adding treesitter context plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,12 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 -- Setup lazy.nvim
 require("lazy").setup({
   'nvim-lua/plenary.nvim',
@@ -246,12 +240,31 @@ require("lazy").setup({
         additional_vim_regex_highlighting = false,
       },
       indent = {
-        enable = true,
+        enable = false,
         disable = { },
       },
     },
   },
   'nvim-treesitter/playground',
+  {
+      'nvim-treesitter/nvim-treesitter-context',
+      dependencies = {
+          'nvim-treesitter/nvim-treesitter',
+      },
+      opts = {
+          enable = true,
+          multiwindow = false,
+          max_lines = 0,
+          max_window_height = 0,
+          line_numbers = true,
+          multiline_threshold = 5,
+          trim_scope = 'outer',
+          mode = 'cursor',
+          separator = nil,
+          zindex = 20,
+          on_attach = nil,
+      },
+  },
 
   -- Colorscheme
   {
